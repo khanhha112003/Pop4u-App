a45ddaaaa588494e28b1f7ee0235a661b8a33b87
AnhThuy <anhthuylqd@gmail.com>
2024-03-23 14:41:19 +0700

    Set up toolbar for search dashboard

--- app/src/main/java/com/group2/pop4u_app/SearchScreen/SearchDashboardActivity.java
+++ app/src/main/java/com/group2/pop4u_app/SearchScreen/SearchDashboardActivity.java
@@ -3,14 +3,21 @@ package com.group2.pop4u_app.SearchScreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import com.group2.pop4u_app.databinding.SearchScreenActivityQuerySearchBinding;
+
+import com.group2.pop4u_app.R;
+import com.group2.pop4u_app.databinding.SearchScreenActivitySearchDashboardBinding;
 
 public class SearchDashboardActivity extends AppCompatActivity {
-    SearchScreenActivityQuerySearchBinding binding;
+    SearchScreenActivitySearchDashboardBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = SearchScreenActivityQuerySearchBinding.inflate(getLayoutInflater());
+        binding = SearchScreenActivitySearchDashboardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        binding.tbToolbarTitle.setTitleTextAppearance(this, R.style.SearchScreenToolbarTitle);
     }
 }
