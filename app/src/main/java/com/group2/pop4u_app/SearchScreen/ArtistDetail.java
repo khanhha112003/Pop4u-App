26cd81a1adb46ab8ed9cb144915a20c31295acd1
ThuyBumr <anhthuylqd@gmail.com>
2024-03-29 15:06:48 +0700

    add artist detail

---
+++ app/src/main/java/com/group2/pop4u_app/SearchScreen/ArtistDetail.java
@@ -0,0 +1,16 @@
+package com.group2.pop4u_app.SearchScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.group2.pop4u_app.R;
+
+public class ArtistDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_detail);
+    }
+}
