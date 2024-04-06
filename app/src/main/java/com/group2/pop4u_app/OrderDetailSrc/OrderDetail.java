6c397767840f82c00b88bc256a5021d87af0c100
Le Thi Thanh Tam <thanhtamdl2701@gmail.com>
2024-04-06 23:34:50 +0700

    fix order detail

---
+++ app/src/main/java/com/group2/pop4u_app/OrderDetailSrc/OrderDetail.java
@@ -0,0 +1,21 @@
+package com.group2.pop4u_app.OrderDetailSrc;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.group2.pop4u_app.R;
+
+public class OrderDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_order_detail);
+    }
+}
