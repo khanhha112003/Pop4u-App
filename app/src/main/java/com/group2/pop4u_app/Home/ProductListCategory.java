03dcfa93173da8a2db09296daae266a069626daa
AnhThuy <anhthuylqd@gmail.com>
2024-04-17 00:44:47 +0700

    Dialog can handle event

--- app/src/main/java/com/group2/pop4u_app/Home/ProductListCategory.java
+++ app/src/main/java/com/group2/pop4u_app/Home/ProductListCategory.java
@@ -14,7 +14,9 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
+import com.google.android.material.slider.RangeSlider;
 import com.group2.adapter.BigProductCardRecyclerAdapter;
 import com.group2.api.Services.ProductService;
 import com.group2.model.Product;
@@ -71,6 +73,85 @@ public class ProductListCategory extends AppCompatActivity {
         Dialog dialog = new Dialog(ProductListCategory.this);
         dialog.setContentView(R.layout.dialog_filter_product);
         dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        dialog.findViewById(R.id.chipAscendPrice).setOnClickListener(v -> {
+            Log.d("Filter", "Ascend Price");
+        });
+
+        dialog.findViewById(R.id.chipDescendPrice).setOnClickListener(v -> {
+            Log.d("Filter", "Descend Price");
+        });
+
+        dialog.findViewById(R.id.chipAllProduct).setOnClickListener(v -> {
+            Log.d("Filter", "All Product");
+        });
+
+        dialog.findViewById(R.id.chipAlbum).setOnClickListener(v -> {
+            Log.d("Filter", "Album");
+        });
+
+        dialog.findViewById(R.id.chipMerch).setOnClickListener(v -> {
+            Log.d("Filter", "Merch");
+        });
+
+        dialog.findViewById(R.id.chipPhotobook).setOnClickListener(v -> {
+            Log.d("Filter", "Photobook");
+        });
+
+        dialog.findViewById(R.id.chipVinyl).setOnClickListener(v -> {
+            Log.d("Filter", "Vinyl");
+        });
+
+        dialog.findViewById(R.id.chipLightstick).setOnClickListener(v -> {
+            Log.d("Filter", "Lightstick");
+        });
+
+        RangeSlider rangeSlider = dialog.findViewById(R.id.sliderYearRangeProductRelease);
+        rangeSlider.addOnChangeListener((slider, value, fromUser) -> {
+            String upperValue = String.valueOf(slider.getValues().get(1).intValue());
+            String lowerValue = String.valueOf(slider.getValues().get(0).intValue());
+            String initRangeValue = String.format("Year Range: %s - %s", lowerValue, upperValue);
+            TextView rangeValue = dialog.findViewById(R.id.txtRangeDateValue);
+            rangeValue.setText(initRangeValue);
+        });
+
+        TextView rangeValue = dialog.findViewById(R.id.txtRangeDateValue);
+        rangeValue.setText(String.format("Year Range: %s - %s",
+                                         String.valueOf(rangeSlider.getValues().get(0).intValue()),
+                                         String.valueOf(rangeSlider.getValues().get(1).intValue())));
+
+        dialog.findViewById(R.id.chipUnder500).setOnClickListener(v -> {
+            Log.d("Filter", "Under 500");
+        });
+
+        dialog.findViewById(R.id.chipUnder1K).setOnClickListener(v -> {
+            Log.d("Filter", "500 to 1000");
+        });
+
+        dialog.findViewById(R.id.chipUnder1_5K).setOnClickListener(v -> {
+            Log.d("Filter", "1000 to 2000");
+        });
+
+        dialog.findViewById(R.id.chipUnder2K).setOnClickListener(v -> {
+            Log.d("Filter", "1000 to 2000");
+        });
+
+        dialog.findViewById(R.id.chipOver2K).setOnClickListener(v -> {
+            Log.d("Filter", "Over 2000");
+        });
+
+        dialog.findViewById(R.id.btnApplyFilter).setOnClickListener(v -> {
+            Log.d("Filter", "Apply Filter");
+            dialog.dismiss();
+        });
+
+        dialog.findViewById(R.id.btnResetFilter).setOnClickListener(v -> {
+            Log.d("Filter", "Reset Filter");
+            rangeSlider.setValues(2010F, 2021F);
+            rangeValue.setText(String.format("Year Range: %s - %s",
+                                             String.valueOf(rangeSlider.getValues().get(0).intValue()),
+                                             String.valueOf(rangeSlider.getValues().get(1).intValue())));
+        });
 
         Window window = dialog.getWindow();
         if (window != null) {
