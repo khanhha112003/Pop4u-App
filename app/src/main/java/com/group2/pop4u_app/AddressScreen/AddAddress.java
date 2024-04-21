7bb3a0b090d1e40876f68940b22e8bcaf3e4fca2
Khanhhane <hapnk21411@st.uel.edu.vn>
2024-04-21 21:39:46 +0700

    sua chut xiu

--- app/src/main/java/com/group2/pop4u_app/AddressScreen/AddAddress.java
+++ app/src/main/java/com/group2/pop4u_app/AddressScreen/AddAddress.java
@@ -3,14 +3,36 @@ package com.group2.pop4u_app.AddressScreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.group2.pop4u_app.R;
+import com.group2.pop4u_app.databinding.ActivityAddAddressBinding;
 
 public class AddAddress extends AppCompatActivity {
-
+ActivityAddAddressBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_address);
+        binding = ActivityAddAddressBinding.inflate(getLayoutInflater());
+        setSupportActionBar(binding.tbrAddAdress);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(binding.getRoot());
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.plain_action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
