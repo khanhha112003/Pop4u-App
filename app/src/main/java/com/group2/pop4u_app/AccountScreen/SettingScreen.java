b27f6a7540006076d03f97f7617848ea4e617a64
Le Thi Thanh Tam <thanhtamdl2701@gmail.com>
2024-04-22 15:58:20 +0700

    terms and help

--- app/src/main/java/com/group2/pop4u_app/AccountScreen/SettingScreen.java
+++ app/src/main/java/com/group2/pop4u_app/AccountScreen/SettingScreen.java
@@ -45,6 +45,12 @@ public class SettingScreen extends AppCompatActivity {
             } else if (settingItem.getSettingID().equals("website")) {
                 replaceFragment(new PaymentMethodFragment());
                 getSupportActionBar().setTitle(settingItem.getSettingTitle());
+            } else if (settingItem.getSettingID().equals("termsAndPolicy")) {
+                replaceFragment(new TermsFragment());
+                getSupportActionBar().setTitle(settingItem.getSettingTitle());
+            } else if (settingItem.getSettingID().equals("helpAndReport")) {
+                replaceFragment(new HelpsNReportFragment());
+                getSupportActionBar().setTitle(settingItem.getSettingTitle());
             }
         } 
     }
